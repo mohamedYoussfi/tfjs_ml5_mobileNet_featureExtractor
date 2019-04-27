10bc9e9be239d6fbf0b9ba91f1cf58cda4446da1
Youssfi <med@youssfi.net>
2019-04-27 08:05:47 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
-
+import {AfterViewInit, Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
+declare let ml5:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'imc-video-ml5';
+export class AppComponent implements OnInit, AfterViewInit{
+  @ViewChild('video')
+  public video : ElementRef;
+  @ViewChild('capture')
+  public captureCanvas : ElementRef;
+  public mobileNetFeatureExtreactor;
+  public featureClassifier;
+  imageLabel: string;
+  public captures=[];
+  public outputLabel;
+  private confidence: number;
+  currentProgress: number=0;
+  private currentLoss: number;
+
+  constructor(private ngZone:NgZone){}
+
+  ngOnInit(): void {
+    this.mobileNetFeatureExtreactor=ml5.featureExtractor('MobileNet',()=>{
+      this.featureClassifier=this.mobileNetFeatureExtreactor.classification(this.video.nativeElement,()=>{
+        console.log(this.featureClassifier);
+      });
+    });
+  }
+  ngAfterViewInit(): void {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+      navigator.mediaDevices.getUserMedia({video:true})
+        .then(stream=>{
+          this.video.nativeElement.srcObject=stream;
+          this.video.nativeElement.play();
+        })
+    }
+  }
+
+  addImage() {
+    this.featureClassifier.addImage(this.imageLabel);
+    this.capture();
+  }
+
+  private capture() {
+    let context=this.captureCanvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement,0,0,320,240);
+    this.captures.push(this.captureCanvas.nativeElement.toDataURL('image/png'));
+  }
+
+  trainModel() {
+    this.featureClassifier.train((loss)=>{
+      if(loss==null){
+        this.mobileNetFeatureExtreactor.classify((e,r)=>{
+          this.getResults(e,r);
+        });
+      }
+      else{
+        this.ngZone.run(()=>{
+          this.currentLoss=loss;
+          ++this.currentProgress;
+        });
+        console.log(loss);
+      }
+    });
+  }
+
+  private getResults(err: any, results: any) {
+    if(err){
+      console.log(err);
+    }
+    else{
+      this.ngZone.run(()=>{
+        this.outputLabel=results[0].label;
+        this.confidence=results[0].confidence;
+      });
+
+    }
+    this.mobileNetFeatureExtreactor.classify((e,r)=>{
+      this.getResults(e,r);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ProgressBarModule} from 'angular-progress-bar';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ProgressBarModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
